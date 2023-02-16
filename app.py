fd475e7a419c059c97db25e52e44225f2c20c1e1
lilyabbruzzese <labbruzz@terpmail.umd.edu>
2023-02-15 20:36:10 -0500

    Committing my about html page.

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello_world():  # put application's code here
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
+
