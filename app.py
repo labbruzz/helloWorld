1a8d557515d3fdc4da6650055ba4d5ea565f2677
lilyabbruzzese <labbruzz@terpmail.umd.edu>
2023-03-30 21:22:00 -0400

    Added new contact and favorite course HTML files and routes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,17 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
